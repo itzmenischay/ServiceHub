3aede963fae83ea7192c40c4ebe6fd852a0d1bdb
Nischay M <gladiatoruk1234@gmail.com>
2026-07-02 01:08:57 +0530

    fix(env url mismatch): fix server.js and app.js frontend url for consistency

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   },
 });
